f14bad2107fa651636c2fe320d23bf7b7cb1f0d2
Weijing Huang <weijinghuang@Weijings-Air.fios-router.home>
2022-10-01 14:48:06 -0400

    changed background

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ document.getElementById('button1').addEventListener('click', ()=>{
     getJoke()
     .then(data => {
         //console.log(data);
-        const question = `<p class="text-white fs-1 fw-bold">${data.setup}</p>`;
+        const question = `<p class="text-white fw-bold">${data.setup}</p>`;
         document.getElementById('output').innerHTML = question;
         document.getElementById('answer').innerHTML = '';
 
         setTimeout(() => {
-            const answer = `<p class="fs-1 fw-bold">${data.punchline}</p>`;
+            const answer = `<p class="fw-bold">${data.punchline}</p>`;
             document.getElementById('answer').innerHTML = answer;
         }, 3000);
     
@@ -30,7 +30,7 @@ function getFunThing() {
     fetch('https://www.boredapi.com/api/activity')
         .then(res => res.json())
         .then(data => {
-            const output = `<p class="fs-1 fw-bold">${data.activity}</p>`
+            const output = `<p class="fw-bold">${data.activity}</p>`
             document.getElementById('output').innerHTML = output;
             document.getElementById('answer').innerHTML = '';
            })
